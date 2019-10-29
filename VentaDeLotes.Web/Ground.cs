b3ed9f2039b5d2889ec20b05a69a07c66608cdbf
ANDREA VIRIDIANA RIOS FERNANDEZ <andreaviridiana.rios@my.uvm.edu.mx>
2019-10-28 21:42:59 -0600

    Se creo la clase "Ground"

---
+++ VentaDeLotes.Web/Ground.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VentaDeLotes.Web
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Ground
+    {
+        [Key]
+        public in GroundId { get; set }
+    [Required(ErrorMessage = "El campo {0} es requerido")]
+    [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracter")]
+    [Index("GroundNameIndez", IsUnique = true)]
+    }
+}
