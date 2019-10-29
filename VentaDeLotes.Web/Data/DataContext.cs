a51c5fe96a1706a8fa4ee6945cdea85a51501936
Saul Macias Zavaleta <saul.maciasz@my.uvm.edu.mx>
2019-10-29 17:10:25 -0600

    importacion de biblioteca

--- VentaDeLotes.Web/Data/DataContext.cs
+++ VentaDeLotes.Web/Data/DataContext.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 namespace VentaDeLotes.Web.Data
 {
     using Microsoft.EntityFrameworkCore;
+    using VentaDeLotes.Web.Entities;
+
     public class DataContext:DbContext
     {
         public DbSet<Ground> Grounds { get; set; }
